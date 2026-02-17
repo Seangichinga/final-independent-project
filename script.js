cc388bc12e3f55145f075df20eee252052450624
Sean Gathecere Gichinga <seangichinga@gmail.com>
2026-02-17 12:11:31 +0300

    Adding more functions

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ function loadData() {
         favorites = JSON.parse(storedFavorites);
     }
 }
+
+function saveData() {   
+    localStorage.setItem('properties', JSON.stringify(allProperties));
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+}
+
+function addProperty(propertyData) {
+    const property = {id: Date.now(),...propertyData,dateAdded: new Date().toLocaleDateString()};
+    allProperties.push(property);
+    saveData();
+    return property;
+}
+
+function toggleFavorite(propertyId) {
+    const index = favorites.indexOf(propertyId);
+    if (index >= 0) {
+        favorites.splice(index, 1);
+    } else {
+        favorites.push(propertyId);
+    }
+    saveData();
+}
+
